2aab5a3220077bcab7533088e560ba89401e9d16
philapulga10 <phinguyenuit10@gmail.com>
2024-05-16 16:55:10 +0700

    handle sizes

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/components/size-form.tsx
@@ -0,0 +1,168 @@
+"use client";
+
+import { useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { z } from "zod";
+import { Size } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Trash } from "lucide-react";
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { AlertModal } from "@/components/modals/alert-modal";
+
+interface SizeFormProps {
+  initialData: Size | null;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  value: z.string().min(1),
+});
+
+type SizeFormValues = z.infer<typeof formSchema>;
+
+export const SizeForm: React.FC<SizeFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const title = initialData ? "Edit size" : "Create size";
+  const description = initialData ? "Edit a size" : "Add a new size";
+  const toastMessage = initialData ? "Size updated" : "Size created";
+  const action = initialData ? "Save changes" : "Create size";
+
+  const form = useForm<SizeFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      value: "",
+    },
+  });
+
+  const onSubmit = async (data: SizeFormValues) => {
+    try {
+      setLoading(true);
+
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/sizes/${params.sizeId}`,
+          data
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/sizes`, data);
+      }
+
+      router.refresh();
+      router.push(`/${params.storeId}/sizes`);
+      toast.success(toastMessage);
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+
+      await axios.delete(`/api/${params.storeId}/sizes/${params.sizeId}`);
+
+      router.refresh();
+      router.push(`/${params.storeId}/sizes`);
+      toast.success("Size deleted.");
+    } catch (error) {
+      toast.error("Make sure you removed all product using this size first.");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        loading={loading}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            variant="destructive"
+            size="icon"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="w-4 h-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Size name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="value"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Value</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Size value"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" disabled={loading} className="ml-auto">
+            {action}
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+    </>
+  );
+};
---
+++ app/(dashboard)/[storeId]/(routes)/sizes/componens/client.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useRouter, useParams } from "next/navigation";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+import { SizeColumn, columns } from "./columns";
+
+interface SizesClientProps {
+  data: SizeColumn[];
+}
+
+export const SizesClient: React.FC<SizesClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Sizes (${data.length})`}
+          description="Manage sizes for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+          <Plus className="mr-2 w-4 h-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" data={data} columns={columns} />
+      <Heading title="API" description="API calls for Sizes" />
+      <Separator />
+      <ApiList entityName="sizes" entityIdName="sizeId" />
+    </>
+  );
+};
